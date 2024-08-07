6741c8e655c871321d1fd28ff83bed3a1dca71bd
Thierry <thierry@gravitymedia.nl>
2024-08-07 16:10:43 +0200

    Configured for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,21 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 export default {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html' // This sets up the fallback for a single-page application
+		}),
+		prerender: {
+			// Add any necessary options here
+		}
 	},
 	preprocess: preprocess({
 		scss: {
-			prependData: `@import 'src/styles/global.scss';` // Optioneel: importeer globale SCSS bestanden
+			prependData: `@import 'src/styles/global.scss';`
 		},
-		postcss: true // Optioneel: voor postprocessing zoals autoprefixer
+		postcss: true
 	})
 };
