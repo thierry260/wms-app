8b1fe430eeedc229f94198086cd307f1d8b72f81
mvereijken <m.vereijken@hotmail.nl>
2024-09-06 15:35:42 +0200

    Re-enabled PWA

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,21 @@
-import { defineConfig } from 'vite';
-import { sveltekit } from '@sveltejs/kit/vite';
-import path from 'path';
+import { defineConfig } from "vite";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { SvelteKitPWA } from "@vite-pwa/sveltekit";
+import path from "path";
 
 const config = {
-  plugins: [sveltekit()],
+  plugins: [sveltekit(), SvelteKitPWA()],
   define: {
-    'process.env.NODE_ENV': '"production"'
+    "process.env.NODE_ENV": '"production"',
   },
   build: {
-    outDir: 'build',
+    outDir: "build",
     rollupOptions: {
       output: {
-        manualChunks: undefined
-      }
-    }
-  }
+        manualChunks: undefined,
+      },
+    },
+  },
 };
 
 export default config;
